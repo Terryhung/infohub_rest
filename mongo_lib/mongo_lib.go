c92ed582d8ea56204c90bc1eec2df8560af02cd1
terryhung <b00201015@gmail.com>
2018-03-01 23:08:09 +0800

    Add image_url_array

--- mongo_lib/mongo_lib.go
+++ mongo_lib/mongo_lib.go
@@ -24,6 +24,7 @@ type Account struct {
 type News struct {
 	Title             string   `json:"title"`
 	Source_name       string   `json:"source_name"`
+	Image_url_array         []string   `json:"image_url_array"`
 	Image_url         string   `json:"image_url"`
 	Like_numbers      int      `json:"link_numbers"`
 	Unlike_numbers    int      `json:"unlink_numbers"`
