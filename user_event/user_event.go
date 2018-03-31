77ea7a79a138362fdcd6771d2a3b86b620371a33
Terryhung <b00201015@gmail.com>
2018-03-31 12:31:55 +0800

    Finish User Event Check

--- user_event/user_event.go
+++ user_event/user_event.go
@@ -6,3 +6,37 @@ type UserEvent struct {
 	Ga_id      string `json:"ga_id"`
 	News_id    string `json:"news_id"`
 }
+
+func (c UserEvent) Check() bool {
+	status := true
+	valid_event_name := map[string]int{
+		"click_profile":     0,
+		"read_article":      1,
+		"read_notification": 1,
+		"comment":           1,
+		"click_like":        1,
+		"browse":            1,
+		"click_keyword":     2,
+		"search":            2,
+		"add_category":      3,
+	}
+	// Check Needed fields: Can not be nil
+	if c.Event_name == "" || c.Ga_id == "" {
+		status = false
+	}
+
+	// Check Event Name Valid or not
+	check_type, ok := valid_event_name[c.Event_name]
+	if !ok {
+		status = false
+	} else {
+		switch check_type {
+		// News id cant not be nil
+		case 1:
+			if c.News_id == "" {
+				status = false
+			}
+		}
+	}
+	return status
+}
