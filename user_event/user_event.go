f23a49e61efafebfd3daa05e1ac92a14a2a5eac9
Terryhung <b00201015@gmail.com>
2018-03-31 10:53:15 +0800

    Add User Event Structure

---
+++ user_event/user_event.go
@@ -0,0 +1,8 @@
+package user_event
+
+type UserEvent struct {
+	Event_name string `json:"event_name"`
+	Info_id    string `json:"info_id"`
+	Ga_id      string `json:"ga_id"`
+	News_id    string `json:"news_id"`
+}
