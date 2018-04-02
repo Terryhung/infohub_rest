509a87d9857c804be87fd6aecd2c442e608e62a6
Terryhung <b00201015@gmail.com>
2018-04-02 09:26:17 +0800

    Add new function for utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
 	"encoding/hex"
 	"hash"
 	"math"
@@ -10,6 +12,11 @@ import (
 func NowTSNorm() int32 {
 	ts := time.Now().Unix()
 	return int32(math.Floor(float64(ts)/86400) * 86400)
+}
+
+func NowTS() int {
+	ts := time.Now().Unix()
+	return int(float64(ts))
 }
 
 func NowMonth() string {
@@ -28,4 +35,14 @@ func MD5SHA1(link string, h hash.Hash, hasher hash.Hash) string {
 	h.Write([]byte(_id))
 	bs := hex.EncodeToString(h.Sum(nil))
 	return bs
+}
+
+func SpecialID(link string) string {
+	h := sha1.New()
+	hasher := md5.New()
+	hasher.Write([]byte(link))
+	_id := hex.EncodeToString(hasher.Sum(nil))
+	h.Write([]byte(_id))
+	bs := hex.EncodeToString(h.Sum(nil))
+	return bs[:24]
 }
